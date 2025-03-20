d171fad9f6f18fc02d18e80222d5819939ccc1a5
e-s-a-h-C <chaseneumann01@gmail.com>
2025-03-20 17:38:32 -0500

    Added to connect app to firebase

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDh4J5ivqMvY6jn1pFqeFV7coekqAanO-I",
+  authDomain: "gravitydenton.firebaseapp.com",
+  databaseURL: "https://gravitydenton-default-rtdb.firebaseio.com",
+  projectId: "gravitydenton",
+  storageBucket: "gravitydenton.firebasestorage.app",
+  messagingSenderId: "620059299581",
+  appId: "1:620059299581:web:812609f110ef78bb85f894"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
